d06def6ce49aa71e20dd6e986e2cd364fc0da85c
fmintus <fmintus@gmail.com>
2021-10-12 12:00:01 +0000

    Update test

--- tests/ai_engine_wrapper/test_ai_engine_infer.py
+++ tests/ai_engine_wrapper/test_ai_engine_infer.py
@@ -1,9 +1,8 @@
 import os
 
-from config.default import get_cfg_from_file as get_cfg_app
-from ai_engine.config.default import get_cfg_from_file as get_cfg_model
+import torch
+from config.default import get_cfg_from_file
 from ai_engine.models import get_model
-from ai_engine.train_utils import get_optimizer, save_checkpoint
 from ai_engine_wrapper.ai_engine_wrapper import ai_engine_infer
 
 import numpy as np
@@ -15,7 +14,7 @@ def build_dummy_infer_raster(model_cfg, savepath):
     input_size = model_cfg.DATASET.SHAPE
     band = np.random.randint(0, 30000, input_size, dtype=np.int16)
 
-    bands_count = len(model_cfg.DATASET.INPUT.USED_CHANNELS)
+    bands_count = max(model_cfg.DATASET.INPUT.USED_CHANNELS) + 1
     img = np.concatenate(bands_count * [np.expand_dims(band, axis=0)], axis=0)
     affine = rio.transform.from_bounds(-10, -10, 10, 10, img.shape[1], img.shape[2])
     with rio.open(
@@ -35,23 +34,24 @@ def build_dummy_infer_raster(model_cfg, savepath):
 
 def test_infer():
 
-    model_config_path = os.path.join("ai_engine", "config", "tests.yml")
     app_cfg_path = os.path.join("config", "default.yml")
     checkpoint_save_path = os.path.join("tests", "ai_engine_wrapper", "test_checkpoint")
     infer_directory = os.path.join("tests", "ai_engine_wrapper", "test_masks")
     tile_path = os.path.join("tests", "ai_engine_wrapper", "test_tile.tiff")
 
-    model_cfg = get_cfg_model(model_config_path)
-    app_cfg = get_cfg_app(app_cfg_path)
+    app_cfg = get_cfg_from_file(app_cfg_path)
 
-    model = get_model(model_cfg, app_cfg.INFER.DEVICE)
-    optimizer = get_optimizer(model, model_cfg)
-    epoch = 1
-    loss = 1.0
+    model = get_model(app_cfg, app_cfg.INFER.DEVICE)
 
-    build_dummy_infer_raster(model_cfg, tile_path)
-    save_checkpoint(model, epoch, optimizer, loss, model_cfg, checkpoint_save_path)
+    build_dummy_infer_raster(app_cfg, tile_path)
 
-    ai_engine_infer(
-        model_config_path, app_cfg, tile_path, checkpoint_save_path, infer_directory
+    torch.save(
+        {
+            "model_state_dict": model.state_dict(),
+        },
+        checkpoint_save_path,
     )
+
+    ai_engine_infer(app_cfg, tile_path, checkpoint_save_path, infer_directory)
+
+test_infer()
